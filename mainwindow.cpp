af38b7f0c7d899d828e2d2cf2a87787b2200d35f
akademik2607 <akademik2607@mail.ru>
2020-05-11 16:52:56 +0300

    Добавил конвртер спецсимволов(#@RUB@)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "myparserspecsymbols.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,24 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::permutationOfSpecSymbols(QString str, const QString &begin, const QString &end){
+    int indB;
+    int indE;
+
+    if(str.isEmpty()) return;
+    MyParserSpecsymbols myParser;
+    indB = myParser.searcher(str, begin, 0);
+    indE = myParser.searcher(str, end, (indB + begin.length()));
+    if(indB == -1 || indE == -1) return;
+     QString makr = str.mid(indB + 2, indE - indB - 2);
+     QString result = myParser.makrPermutation(str, makr, indB, indE);
+     ui->plainTextEdit->setPlainText(result);
+}
+
+void MainWindow::on_plainTextEdit_textChanged()
+{
+    QString str = ui->plainTextEdit->toPlainText();
+    permutationOfSpecSymbols(str, "#@", "@");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,12 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_plainTextEdit_textChanged();
+
 private:
     Ui::MainWindow *ui;
+    void permutationOfSpecSymbols(QString str, const QString &begin, const QString &end);
 };
 
 #endif // MAINWINDOW_H
